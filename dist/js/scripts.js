39cbdadbf4f8ae2edce804a45a0705d9beff1ef1
camiaufranc <camiloaufranc7@gmail.com>
2022-08-15 14:27:54 -0300

    cambios portfolio y boton

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -71,3 +71,44 @@ document.getElementById('contactForm')
       alert(JSON.stringify(err));
     });
 });
+
+
+//Iniciar las alturas máxima y mínima del bloque contenedor.
+//mínima es con el texto oculto, máxima con el texto visible.
+var caja = document.getElementById("caja");
+var moreText = document.getElementById("more");
+caja.max = caja.offsetHeight + 'px';
+moreText.style.display = 'none';
+caja.min = caja.offsetHeight + 'px';
+caja.style.height = caja.min;
+caja.style.transition = "height 1500ms";
+//Esta función cambia la altura y oculta los botones si está abriendo
+//y muestra el texto completo
+function toggle() {
+  var dots = document.getElementById("dots");
+  if (caja.style.height != caja.min && caja.style.height != '') {
+    caja.style.height = caja.min;
+  } else {
+    dots.style.display = "none";
+    caja.style.height = caja.max;
+    moreText.style.display = "inline";
+  }
+}
+
+//Función para el evento fin de transición 
+function cambiarAlto() {
+  var dots = document.getElementById("dots");
+  var btnText = document.getElementById("myBtn");
+  //Si está cerrando el botón pone Read less  
+  //Tb se puede comprobar con la altura de caja.
+  if (btnText.innerHTML == "¡Te dije que NO!") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "No hagas click";
+    moreText.style.display = "none";
+  } else {
+    btnText.innerHTML = "¡Te dije que NO!";
+    moreText.style.display = "inline";
+  }
+}
+//asigna el evento, por esto debe estar al final
+document.getElementById("caja").addEventListener("transitionend", cambiarAlto, false);
